40b8298901e57f818e024c095240e23843ef196f
omar <Omar.Bernard64@gmail.com>
2023-04-28 01:06:48 +0000

    updates

---
+++ 2010c3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Omar Bernard
+# Date of latest revision: 4/27/23
+# Purpose: Print a string to the terminal
+
+sudo last/var/log/wtmp
+
+echo "this is the login history."
+
+sudo last -3
