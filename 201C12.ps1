cf7c65ebc47db2015611a8e434fdc995447d30fa
clivebernard <Omar.bernard64@gmail.com>
2023-05-16 19:39:46 -0400

    update

---
+++ 201C12.ps1
@@ -0,0 +1,16 @@
+# Script: Ops 201 Class 12 Ops Challenge Solution
+# Author: Omar Bernard
+# Date of latest revision: 05/15/23
+# Purpose: Print a string to the terminal
+
+# Main
+
+function IP {
+    ipconfig /all | Out-File -FilePath $file
+}
+$file= "C:\Users\cyber\OneDrive\Desktop\network_report.txt"
+IP
+Select-String -Path $file -Pattern IPv4
+Remove-Item -Path $file
+
+# End
