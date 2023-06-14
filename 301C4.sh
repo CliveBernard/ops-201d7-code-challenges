a6fa8decd0c201e9d725d28af5c3073c25fe03f8
clivebernard <Omar.bernard64@gmail.com>
2023-06-13 20:18:19 -0400

    update

---
+++ 301C4.sh
@@ -0,0 +1,34 @@
+!/bin/bash
+# Script: Ops 301 Class 04 Ops Challenge Solution
+# Author: Omar
+# Date of latest revision: 06/13/23
+# Conditionals in Menu Systems
+# Main
+
+e=y
+while [ $e = y ]
+do
+
+echo "Please choose:"
+echo "1) Hello world!"
+echo "2) Ping Self"
+echo "3) IP Info"
+echo "4) Exit"
+read a
+
+if [ $a = 1 ]
+    then echo "Hello world!"
+elif [ $a = 2 ]
+    then ping -c 3 localhost
+elif [ $a = 3 ]
+    then ifconfig
+else [ $a = 4 ]
+    exit
+fi
+
+
+echo "Do you want to try again, y or n?"
+read e
+
+done
+#End
