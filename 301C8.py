cd55e0867abf858db177b2e6b538af309abc1293
clivebernard <Omar.bernard64@gmail.com>
2023-06-26 21:04:21 -0400

    update

---
+++ 301C8.py
@@ -0,0 +1,12 @@
+
+
+my_list = ["RB26DETT","2JZ-GTE","BMW B58","JAG XK6","BMW M54","BMW N55","JEEP-VA31","4G63","BMW M88","RB20DET"]
+
+fourthelement = my_list[3]
+print(fourthelement)
+
+sixelement = my_list[5]
+print(sixelement)
+
+my_list[6] = 'onion'
+print(my_list)
